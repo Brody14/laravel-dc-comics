d4530595ffeb32b57f13533cf2c747a19a485174
Brody14 <stefania.spinas@gmail.com>
2023-04-14 16:00:02 +0200

    FEAT index route

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('content')
+    
+    <div class="container py-5">
+
+        <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">Titolo</th>
+                <th scope="col">Descrizione</th>
+                <th scope="col">Copertina</th>
+                <th scope="col">Prezzo</th>
+                <th scope="col">Serie</th>
+                <th scope="col">Uscita</th>
+                <th scope="col">Tipologia</th>
+                <th scope="col">More</th>
+              </tr>
+            </thead>
+            <tbody>
+                @foreach ($comics as $comic)
+                    <tr>
+                    <td> {{$comic->title}} </td>
+                    <td> {{ substr($comic->description, 0, 150) . '...'}} </td>
+                    <td> <img class="thumb" src="{{$comic->thumb}}" alt=""> </td>
+                    <td> {{$comic->price}} &dollar; </td>
+                    <td> {{$comic->series}} </td>
+                    <td> {{$comic->sale_date}} </td>
+                    <td> {{$comic->type}} </td>
+                    <td class="text-center"> <a href="#">--></a> </td>
+                    </tr>
+                    
+                @endforeach
+              
+            </tbody>
+          </table>
+
+
+    </div>
+    
+@endsection
+ 
+
