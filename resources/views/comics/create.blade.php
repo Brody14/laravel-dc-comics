97d4870b3357c1dd92e7814cda2561daed0e1715
Brody14 <stefania.spinas@gmail.com>
2023-04-14 16:44:01 +0200

    FEAT create

---
+++ resources/views/comics/create.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="container py-5">
+        <h4>Inserisci un nuovo fumetto</h4>
+    </div>
+
+    <div class="container py-3">
+      <form action="">
+
+      </form>
+    </div>
+
+    <div class="container">
+        <button class="btn btn-primary mb-3" type="submit">Invia</button>
+    </div>
+
+@endsection
