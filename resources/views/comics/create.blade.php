a106d5f99c3964842eb44637ffe2e7d9b1966f2d
Brody14 <stefania.spinas@gmail.com>
2023-04-19 17:39:08 +0200

    EDIT repopulating create form

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -13,7 +13,7 @@
 
         <div class="mb-3">
             <label for="title" class="form-label">Titolo</label>
-            <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{ old('title') }}">
+            <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{ old('title') ?? "" }}">
             @error('title')
               <div class="invalid-feedback">
                 {{ $message }}
@@ -23,7 +23,7 @@
 
           <div class="mb-3">
             <label for="thumb" class="form-label">Copertina (url)</label>
-            <input type="text" class="form-control @error('thumb') is-invalid @enderror" id="thumb" name="thumb" value="{{ old('thumb') }}">
+            <input type="text" class="form-control @error('thumb') is-invalid @enderror" id="thumb" name="thumb" value="{{ old('thumb') ?? "" }}">
             @error('thumb')
               <div class="invalid-feedback">
                 {{ $message }}
@@ -33,7 +33,7 @@
     
           <div class="mb-3">
             <label for="description" class="form-label">Descrizione</label>
-            <textarea class="form-control @error('description') is-invalid @enderror" name="description" id="description" cols="30" rows="10">{{ old('description') }}</textarea>
+            <textarea class="form-control @error('description') is-invalid @enderror" name="description" id="description" cols="30" rows="10">{{ old('description') ?? "" }}</textarea>
             @error('description')
               <div class="invalid-feedback">
                 {{ $message }}
@@ -43,7 +43,7 @@
 
           <div class="mb-3">
             <label for="price" class="form-label">Prezzo</label>
-            <input type="text" class="form-control @error('price') is-invalid @enderror" id="price" name="price" value="{{ old('price') }}">
+            <input type="text" class="form-control @error('price') is-invalid @enderror" id="price" name="price" value="{{ old('price') ?? "" }}">
             @error('price')
               <div class="invalid-feedback">
                 {{ $message }}
@@ -54,7 +54,7 @@
     
           <div class="mb-3">
             <label for="series" class="form-label">Serie</label>
-            <input type="text" class="form-control @error('series') is-invalid @enderror" id="series" name="series" value="{{ old('series') }}">
+            <input type="text" class="form-control @error('series') is-invalid @enderror" id="series" name="series" value="{{ old('series') ?? "" }}">
             @error('series')
               <div class="invalid-feedback">
                 {{ $message }}
@@ -64,7 +64,7 @@
     
           <div class="mb-3">
             <label for="sale-date" class="form-label">Uscita</label>
-            <input type="date" class="form-control @error('sale_date') is-invalid @enderror" id="sale-date" name="sale_date" value="{{ old('sale_date') }}">
+            <input type="date" class="form-control @error('sale_date') is-invalid @enderror" id="sale-date" name="sale_date" value="{{ old('sale_date') ?? "" }}">
             @error('sale_date')
               <div class="invalid-feedback">
                 {{ $message }}
